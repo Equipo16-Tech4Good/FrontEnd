edb873bbabbcfca67d69a741d31b6f07051dc8e6
alissiafrolova <frolova.alissia@gmail.com>
2023-06-20 15:14:01 +0200

    sign_up

--- src/app/authorization/sign-up/sign-up.component.ts
+++ src/app/authorization/sign-up/sign-up.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
+import { GlobalConstants } from '../../shared/globar_constants';
 
 @Component({
   selector: 'app-sign-up',
@@ -7,9 +9,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SignUpComponent implements OnInit {
 
-  constructor() { }
+  password = true;
+  confirmPassword = true;
+  signupFrom:any = FormGroup;
+  responseMessage:any;
+
+  //falta importar el service y añadirlo en el constructor
+
+  constructor(private formBuilder:FormBuilder) { }
 
   ngOnInit(): void {
+    this.signupFrom = this.formBuilder.group({
+      name: [null, [Validators.required, Validators.pattern(GlobalConstants.nameRegex)]],
+      email: [null, [Validators.required, Validators.pattern(GlobalConstants.emailRegex)]],
+      password: [null, [Validators.required, Validators.minLength(5), Validators.maxLength(20)]],
+      confirmPassword: [null, [Validators.required]]
+    });
   }
 
+  validateSubmit(){
+    if(this.signupFrom.controls['password'].value != this.signupFrom.controls['confirmPassword'].value)
+      return true;
+    else
+      return false;
+  }
 }
